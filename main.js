33f69aeb3e1347ff5cbcd567035fd6c70d696900
Pedro Henrique Gondim <107958899+Pedrohdsg@users.noreply.github.com>
2023-04-10 21:31:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '15c04215dfmsh47b152db5b5c176p1edfc4jsn455d1e039959',
+// 		'X-RapidAPI-Host': 'rock-paper-scissors13.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://rock-paper-scissors13.p.rapidapi.com/?choice=paper', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
