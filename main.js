683e2079fe5f8846218fe10fbab2d9ddc4a0407b
Pedro Henrique Gondim <107958899+Pedrohdsg@users.noreply.github.com>
2023-04-13 21:21:23 -0300

    Add files via upload
    Melhorias no código

--- main.js
+++ main.js
@@ -2,30 +2,26 @@ let vitorias = 0;
 let derrotas = 0;
 let empates = 0;
 
-
-
 let valorJogador = "";
 let valorAdversario = "";
 
 function jogador() {
   let opcaoJogador = document.getElementById("seletorId").value;
+  const e = document.createElement("p");
 
   if (opcaoJogador === "papel") {
-    const e = document.createElement("p");
     document.getElementById("jogadorId").appendChild(e).innerHTML = `
       <h3>Jogador</h3>
       <img id="bg-jogador" src="img/paper1.png" alt="">
       `;
     valorJogador = "papel";
   } else if (opcaoJogador === "pedra") {
-    const e = document.createElement("p");
     document.getElementById("jogadorId").appendChild(e).innerHTML = `
       <h3>Jogador</h3>
       <img id="bg-jogador" src="img/rock1.png" alt="">
       `;
     valorJogador = "pedra";
   } else {
-    const e = document.createElement("p");
     document.getElementById("jogadorId").appendChild(e).innerHTML = `
       <h3>Jogador</h3>
       <img id="bg-jogador" src="img/scissor1.png" alt="">
@@ -36,23 +32,21 @@ function jogador() {
 
 function sorteioAdversario() {
   let adversario = Math.round(Math.random() * 2);
+  const ad = document.createElement("p");
 
   if (adversario == 0) {
-    const ad = document.createElement("p");
     document.getElementById("adversarioId").appendChild(ad).innerHTML = `
       <h3>Adversário</h3>
       <img id="bg-adversario" src="img/paper2.png" alt="">
       `;
     valorAdversario = "papel";
   } else if (adversario == 1) {
-    const ad = document.createElement("p");
     document.getElementById("adversarioId").appendChild(ad).innerHTML = `
       <h3>Adversário</h3>
       <img id="bg-adversario" src="img/rock2.png" alt="">
       `;
     valorAdversario = "pedra";
   } else {
-    const ad = document.createElement("p");
     document.getElementById("adversarioId").appendChild(ad).innerHTML = `
       <h3>Adversário</h3>
       <img id="bg-adversario" src="img/scissor2.png" alt="">
@@ -75,19 +69,16 @@ function resultados() {
   <h1 class="resultado">Venceu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
-
   } else if (valorJogador == "pedra" && valorAdversario == "papel") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
   <h1 class="resultado">Perdeu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
-
   } else if (valorJogador == "pedra" && valorAdversario == "pedra") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
   <h1 class="resultado">Empate!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
-
   }
 
   if (valorJogador == "papel" && valorAdversario == "tesoura") {
@@ -95,19 +86,16 @@ function resultados() {
   <h1 class="resultado">Perdeu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
-
   } else if (valorJogador == "papel" && valorAdversario == "papel") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
     <h1 class="resultado">Empate!</h1>
     <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
     `;
-
   } else if (valorJogador == "papel" && valorAdversario == "pedra") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
   <h1 class="resultado">Venceu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
-
   }
 
   if (valorJogador == "tesoura" && valorAdversario == "tesoura") {
@@ -115,23 +103,20 @@ function resultados() {
   <h1 class="resultado">Empate!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
-
   } else if (valorJogador == "tesoura" && valorAdversario == "papel") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
   <h1 class="resultado">Venceu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
-
   } else if (valorJogador == "tesoura" && valorAdversario == "pedra") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
   <h1 class="resultado">Perdeu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
-
   }
 
-  const esconderSeletor = document.getElementById('selecaoOpcao')
-  esconderSeletor.style.display = 'none'
+  const esconderSeletor = document.getElementById("selecaoOpcao");
+  esconderSeletor.style.display = "none";
 }
 
 function recomecar() {
