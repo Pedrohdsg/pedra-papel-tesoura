ac08b1852d99d6fed9028643f2dc2af2dfcd3b64
Pedro Henrique Gondim <107958899+Pedrohdsg@users.noreply.github.com>
2023-04-14 17:07:42 -0300

    Add files via upload
    Contagem de derrotas, empates e vitórias adicionado

--- main.js
+++ main.js
@@ -2,8 +2,23 @@ let vitorias = 0;
 let derrotas = 0;
 let empates = 0;
 
+vitorias = JSON.parse(localStorage.getItem('vitorias'))
+derrotas = JSON.parse(localStorage.getItem('derrotas'))
+empates = JSON.parse(localStorage.getItem('empates'))
+
+
 let valorJogador = "";
 let valorAdversario = "";
+
+
+function adicionarPlacar(){
+  let placar = document.createElement('div');
+  document.getElementById('placarId').appendChild(placar).innerHTML =`
+  <p>VITÓRIAS = ${vitorias}_________EMPATES = ${empates}_________DERROTAS = ${derrotas}</p>
+  `
+}
+
+
 
 function jogador() {
   let opcaoJogador = document.getElementById("seletorId").value;
@@ -59,6 +74,7 @@ function jogar() {
   jogador();
   sorteioAdversario();
   resultados();
+  adicionarPlacar()
 }
 
 function resultados() {
@@ -69,16 +85,19 @@ function resultados() {
   <h1 class="resultado">Venceu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
+    vitorias++;
   } else if (valorJogador == "pedra" && valorAdversario == "papel") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
   <h1 class="resultado">Perdeu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
+    derrotas++;
   } else if (valorJogador == "pedra" && valorAdversario == "pedra") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
   <h1 class="resultado">Empate!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
+    empates++;
   }
 
   if (valorJogador == "papel" && valorAdversario == "tesoura") {
@@ -86,16 +105,19 @@ function resultados() {
   <h1 class="resultado">Perdeu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
+    derrotas++;
   } else if (valorJogador == "papel" && valorAdversario == "papel") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
     <h1 class="resultado">Empate!</h1>
     <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
     `;
+    empates++;
   } else if (valorJogador == "papel" && valorAdversario == "pedra") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
   <h1 class="resultado">Venceu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
+    vitorias++;
   }
 
   if (valorJogador == "tesoura" && valorAdversario == "tesoura") {
@@ -103,17 +125,25 @@ function resultados() {
   <h1 class="resultado">Empate!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
+    empates++;
   } else if (valorJogador == "tesoura" && valorAdversario == "papel") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
   <h1 class="resultado">Venceu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
+    vitorias++;
   } else if (valorJogador == "tesoura" && valorAdversario == "pedra") {
     document.getElementById("resultadoId").appendChild(resultado).innerHTML = `
   <h1 class="resultado">Perdeu!</h1>
   <input type="button" value="OK!" id="buttonId" onclick="recomecar()">
   `;
+    derrotas++;
   }
+
+  localStorage.setItem("vitorias", vitorias);
+  localStorage.setItem("derrotas", derrotas);
+  localStorage.setItem("empates", empates);
+
 
   const esconderSeletor = document.getElementById("selecaoOpcao");
   esconderSeletor.style.display = "none";
@@ -121,4 +151,5 @@ function resultados() {
 
 function recomecar() {
   location.reload();
+  
 }
